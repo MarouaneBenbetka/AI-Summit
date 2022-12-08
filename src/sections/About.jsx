486a7820b5bcabb26896ced0bcf34d5903b44a60
MAHDJOUR OUSSAMA <mahdjouroussama@gmail.com>
2022-12-09 00:06:29 +0100

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -4,7 +4,7 @@ import About_element from "../components/about/about_element";
 const About = () => {
 	return <section id="about" className="container h-screen mx-auto flex flex-col justify-center ">
 		<h1 className="py-3 text-4xl leading-10 font-extrabold text-center md:text-left">About</h1>
-		<div className="md:flex space-x-0  md:space-x-4 md:space-y-0 space-y-4	">
+		<div className="md:flex space-x-0  md:space-x-7 md:space-y-0 space-y-7	">
 		<About_element 
 			paragraph="School of AI Algeirs is a local community  of developers students and lovers of Artificiel  Intelligence in Algeria, at the higher school  of  computer science ( ESI Algeirs ). "
 			img={soai_logo}
