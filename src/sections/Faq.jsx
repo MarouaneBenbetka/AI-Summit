fc8b12157d52fc54bce09d6a797a02747a35d217
meryeemdifangel <jm_dif@esi.dz>
2022-12-09 22:53:28 +0100

    faq

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,5 +1,46 @@
 const Faq = () => {
-	return <section id="faq">Faq</section>;
+	return <section  id="faq">
+		 <div className="w-full px-20 mx-auto mt-20 space-y-8   lg:max-w-2xl ">
+                <details className="p-4 rounded-lg cursor-pointer bg-[#F8F8F8]">
+                    <summary className="font-semibold text-gray-800">What is artificial intelligence ?</summary>
+                    <div className="mt-3">
+                        <p className="text-sm leading-6 text-gray-600">
+						School Of AI Algeirs Is A Local Community Of Developers Students And Lovers Of Artificiel Intelligence In Algeria, At The Higher School Of Computer Science ( ESI Algeirs ).
+					 </p>
+                    </div>
+                </details>
+                <details className="p-4 rounded-lg cursor-pointer bg-[#F8F8F8]">
+                    <summary className="font-semibold text-gray-800">
+					What is artificial intelligence ?
+                    </summary>
+                    <div className="mt-3">
+                        <p className="text-sm leading-6 text-gray-600">
+						School Of AI Algeirs Is A Local Community Of Developers Students And Lovers Of Artificiel Intelligence In Algeria, At The Higher School Of Computer Science ( ESI Algeirs ).
+                        </p>
+                    </div>
+                </details>
+                <details className="p-4 rounded-lg text-gray-800 cursor-pointer bg-[#F8F8F8]">
+                    <summary className="font-semibold">
+					What is artificial intelligence ?
+                    </summary>
+                    <div className="mt-3">
+                        <p className="text-sm leading-6 text-gray-600">
+						School Of AI Algeirs Is A Local Community Of Developers Students And Lovers Of Artificiel Intelligence In Algeria, At The Higher School Of Computer Science ( ESI Algeirs ).
+                        </p>
+                    </div>
+                </details>
+                <details className="p-4 rounded-lg text-gray-800 cursor-pointer bg-[#F8F8F8]">
+                    <summary className="font-semibold">
+					What is artificial intelligence ?
+                    </summary>
+                    <div className="mt-3">
+                        <p className="text-sm leading-6 text-gray-600">
+						School Of AI Algeirs Is A Local Community Of Developers Students And Lovers Of Artificiel Intelligence In Algeria, At The Higher School Of Computer Science ( ESI Algeirs ).
+                        </p>
+                    </div>
+                </details>
+            </div>
+	</section>;
 };
 
 export default Faq;
--- src/sections/index.js
+++ src/sections/index.js
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import About from "./About";
 import Agenda from "./Agenda";
 import Speakers from "./Speakers";
+import Faq from "./Faq";
 import Footer from "./Footer";
 
-export { Navbar, Hero, About, Agenda, Speakers, Footer };
+export { Navbar, Hero, About, Agenda, Speakers,Faq , Footer };
