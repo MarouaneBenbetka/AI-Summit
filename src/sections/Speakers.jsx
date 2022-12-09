958a7f6b6e48bc0b1e5daa368618c4cc19038b95
rd07 <rd07gamer@gmail.com>
2022-12-09 01:30:30 +0100

    feat: added cards and cards container

--- src/sections/Speakers.jsx
+++ src/sections/Speakers.jsx
@@ -1,5 +1,34 @@
+import { speakers } from "../data/data.jsx";
+
 const Speakers = () => {
-	return <section id="speakers">Speakers</section>;
+  return (
+    <div className="p-14 flex flex-col font-libre-franklin">
+      <h1 className="text-4xl font-bold ">Speakers</h1>
+      <div className="flex justify-around w-full flex-wrap mt-2 ">
+        {speakers.map((speaker) => (
+          <div
+            key={speaker.id}
+            className=" rounded-xl m-4 w-[260px] bg-white hover:mx-8 transition-all duration-300 "
+          >
+            <img
+              className="rounded-xl h-52"
+              src={speaker.img}
+              alt={speaker.name}
+            />
+            <div className="p-3 mb-2">
+              <h3 className="text-lg font-bold text-blue">{speaker.name}</h3>
+              <p className="text-sm font-light text-red pb-1">
+                {speaker.profession}
+              </p>
+              <p className="text-gray-600 font-bold text-sm ">
+                {speaker.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Speakers;
