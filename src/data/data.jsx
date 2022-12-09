cbf06df0eac25ee6b731397fe56109ed0ae60067
Marouane <MarouaneBenbetka@gmail.com>
2022-12-09 10:58:53 +0100

    layout modification -speakers section-

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,121 +1,121 @@
 import {
-  FaFacebook,
-  FaLinkedin,
-  FaTwitter,
-  FaYoutube,
-  FaInstagram,
+	FaFacebook,
+	FaLinkedin,
+	FaTwitter,
+	FaYoutube,
+	FaInstagram,
 } from "react-icons/fa";
 
 import speaker_img from "../assets/cool_speaker.jpg";
 
 export const navLinks = [
-  {
-    id: "home",
-    title: "Home",
-  },
-  {
-    id: "about",
-    title: "About",
-  },
-  {
-    id: "speakers",
-    title: "Speakers",
-  },
-  {
-    id: "agenda",
-    title: "Agenda",
-  },
-  {
-    id: "faq",
-    title: "Faq",
-  },
+	{
+		id: "home",
+		title: "Home",
+	},
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "speakers",
+		title: "Speakers",
+	},
+	{
+		id: "agenda",
+		title: "Agenda",
+	},
+	{
+		id: "faq",
+		title: "Faq",
+	},
 ];
 
 //list of speakers for the speakers section
 export const speakers = [
-  {
-    id: 1,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 2,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 3,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 4,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 5,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 6,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 7,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
-  {
-    id: 8,
-    name: "Aouinane Moussa",
-    profession: "Data Scientist",
-    description: "Ecole Nationale D'informatique",
-    img: speaker_img,
-  },
+	{
+		id: 1,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 2,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 3,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 4,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 5,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 6,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 7,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
+	{
+		id: 8,
+		name: "Aouinane Moussa",
+		profession: "Data Scientist",
+		description: "Ecole Nationale D'informatique",
+		img: speaker_img,
+	},
 ];
 
 //social media list of components
 export const socials = [
-  {
-    name: "Facebook",
-    link: "https://www.facebook.com/SchoolofAIAlgiers/",
-    icon: <FaFacebook size={30}></FaFacebook>,
-  },
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/soai_algiers",
-    icon: <FaInstagram size={30}></FaInstagram>,
-  },
-  {
-    name: "Linkedin",
-    link: "https://www.linkedin.com/company/school-of-ai-algiers/",
-    icon: <FaLinkedin size={30}></FaLinkedin>,
-  },
-  {
-    name: "Twitter",
-    link: "https://twitter.com/soaialgiers",
-    icon: <FaTwitter size={30}></FaTwitter>,
-  },
-  {
-    name: "Youtube",
-    link: "https://m.youtube.com/channel/UChFa3XnZYZ94tkjfJ5CnPOA",
-    icon: <FaYoutube size={30}></FaYoutube>,
-  },
+	{
+		name: "Facebook",
+		link: "https://www.facebook.com/SchoolofAIAlgiers/",
+		icon: <FaFacebook size={30}></FaFacebook>,
+	},
+	{
+		name: "Instagram",
+		link: "https://www.instagram.com/soai_algiers",
+		icon: <FaInstagram size={30}></FaInstagram>,
+	},
+	{
+		name: "Linkedin",
+		link: "https://www.linkedin.com/company/school-of-ai-algiers/",
+		icon: <FaLinkedin size={30}></FaLinkedin>,
+	},
+	{
+		name: "Twitter",
+		link: "https://twitter.com/soaialgiers",
+		icon: <FaTwitter size={30}></FaTwitter>,
+	},
+	{
+		name: "Youtube",
+		link: "https://m.youtube.com/channel/UChFa3XnZYZ94tkjfJ5CnPOA",
+		icon: <FaYoutube size={30}></FaYoutube>,
+	},
 ];
--- src/sections/Speakers.jsx
+++ src/sections/Speakers.jsx
@@ -1,34 +1,19 @@
+import SpeakerCard from "../components/speakers/SpeakerCard.jsx";
 import { speakers } from "../data/data.jsx";
 
 const Speakers = () => {
-  return (
-    <div className="p-14 flex flex-col font-libre-franklin">
-      <h1 className="text-4xl font-bold text-center md:text-left">Speakers</h1>
-      <div className="flex justify-around w-full flex-wrap mt-2 ">
-        {speakers.map((speaker) => (
-          <div
-            key={speaker.id}
-            className=" rounded-xl m-4 w-[260px] bg-white hover:mx-8 transition-all duration-300 "
-          >
-            <img
-              className="rounded-xl h-52"
-              src={speaker.img}
-              alt={speaker.name}
-            />
-            <div className="p-3 mb-2">
-              <h3 className="text-lg font-bold text-blue">{speaker.name}</h3>
-              <p className="text-sm font-light text-red pb-1">
-                {speaker.profession}
-              </p>
-              <p className="text-gray-600 font-bold text-sm ">
-                {speaker.description}
-              </p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="py-14 px-20 lg:px-[12vw]  flex flex-col font-libre-franklin">
+			<h1 className="text-4xl font-bold text-center md:text-left">
+				Speakers
+			</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 mt-3 ">
+				{speakers.map((speaker) => (
+					<SpeakerCard speaker={speaker} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Speakers;
