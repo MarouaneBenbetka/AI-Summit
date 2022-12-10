5045ed979abf7dbcf1052e2359730d6bcadb5d51
moonayyur <jm_kias@esi.dz>
2022-12-10 23:28:19 +0100

    Remove warnings

--- src/components/hero/particules.jsx
+++ src/components/hero/particules.jsx
@@ -3,96 +3,95 @@ import { loadFull } from "tsparticles";
 import { useCallback } from "react";
 
 const Particlesbg = () => {
-	const particlesInit = useCallback(async (engine) => {
-		console.log(engine);
-		// you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-		// starting from v2 you can add only the features you need reducing the bundle size
-		await loadFull(engine);
-	}, []);
+  const particlesInit = useCallback(async (engine) => {
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
 
-	return (
-		<Particles
-			className="h-[80vh] min-h-[500px] w-screen absolute"
-			id="tsparticles"
-			init={particlesInit}
-			options={{
-				background: {
-					color: {
-						value: "#1B1E21",
-					},
-				},
-				fpsLimit: 120,
-				interactivity: {
-					events: {
-						onClick: {
-							enable: false,
-							mode: "push",
-						},
-						onHover: {
-							enable: true,
-							mode: "repulse",
-						},
-						resize: true,
-					},
-					modes: {
-						push: {
-							quantity: 1,
-						},
-						repulse: {
-							distance: 100,
-							duration: 0.8,
-						},
-					},
-				},
-				particles: {
-					color: {
-						value: "#CC2429",
-					},
-					links: {
-						color: "#CCD9DA",
-						distance: 150,
-						enable: true,
-						opacity: 0.3,
-						width: 1,
-					},
-					collisions: {
-						enable: false,
-					},
-					move: {
-						directions: "none",
-						enable: true,
-						outModes: {
-							default: "bounce",
-						},
-						random: false,
-						speed: 6,
-						straight: false,
-					},
-					number: {
-						density: {
-							enable: true,
-							area: 800,
-						},
-						value: 80,
-					},
-					opacity: {
-						value: 0.5,
-					},
-					shape: {
-						type: "circle",
-					},
-					size: {
-						value: { min: 1, max: 6 },
-					},
-				},
-				detectRetina: true,
-				fullScreen: {
-					enable: false,
-				},
-			}}
-		/>
-	);
+  return (
+    <Particles
+      className="h-[80vh] min-h-[500px] w-screen absolute"
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        background: {
+          color: {
+            value: "#1B1E21",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 1,
+            },
+            repulse: {
+              distance: 100,
+              duration: 0.8,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#CC2429",
+          },
+          links: {
+            color: "#CCD9DA",
+            distance: 150,
+            enable: true,
+            opacity: 0.3,
+            width: 1,
+          },
+          collisions: {
+            enable: false,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 6 },
+          },
+        },
+        detectRetina: true,
+        fullScreen: {
+          enable: false,
+        },
+      }}
+    />
+  );
 };
 
 export default Particlesbg;
--- src/sections/Speakers.jsx
+++ src/sections/Speakers.jsx
@@ -2,21 +2,19 @@ import SpeakerCard from "../components/speakers/SpeakerCard.jsx";
 import { speakers } from "../data/data.jsx";
 
 const Speakers = () => {
-	return (
-		<section
-			id="speakers"
-			className="py-14 px-8 sm:px-20 lg:px-[12vw] pt-[84px]  flex flex-col font-libre-franklin"
-		>
-			<h1 className="text-4xl font-bold text-center md:text-left">
-				Speakers
-			</h1>
-			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 mt-3 ">
-				{speakers.map((speaker) => (
-					<SpeakerCard speaker={speaker} />
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section
+      id="speakers"
+      className="py-14 px-8 sm:px-20 lg:px-[12vw] pt-[84px]  flex flex-col font-libre-franklin"
+    >
+      <h1 className="text-4xl font-bold text-center md:text-left">Speakers</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-7 mt-3 ">
+        {speakers.map((speaker) => (
+          <SpeakerCard speaker={speaker} key={speaker.id} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Speakers;
