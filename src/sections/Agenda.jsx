06c69fcdeb8ac817188bcc7a2c787748d5b565f8
MoncefME <ka_moussaoui@esi.dz>
2022-12-10 15:17:36 +0100

    work in progress

--- src/sections/Agenda.jsx
+++ src/sections/Agenda.jsx
@@ -1,22 +1,32 @@
 import { agenda } from "../data/data";
-import {BiTimeFive} from "react-icons/bi"
+import { BiTimeFive } from "react-icons/bi";
+import agenda_neurons from "../assets/agenda_neurons.svg";
 
 const Agenda = () => {
   return (
     <div className="py-14 px-20 ">
       <h1 className="text-4xl font-bold text-center md:text-left">Agenda</h1>
+	
+      <div className="flex items-center gap-0 ">
+        <div className="h-1 bg-[#5D2024] w-full mx-0" />
+        <BiTimeFive
+          size={50}
+          color="#5D2024"
+          className="h-fit overflow-visible"
+        />
+        <div className="h-1 bg-[#5D2024] w-full" />
+      </div>
 
-	  <div className="flex items-center gap-0 ">
-		<div className="h-1 bg-[#5D2024] w-full mx-0"/>
-		<BiTimeFive size={50} color="#5D2024" className="h-fit px-[-2]  overflow-visible"/>
-		<div className="h-1 bg-[#5D2024] w-full"/>
-	  </div>
-
-      <div className="bg-slate-600 py-5">
+      <div className="py-5 flex flex-col gap-3 relative">
+        <img
+          src={agenda_neurons}
+          alt="React Logo"
+          className="invisible md:visible sm:visible h-4/5 w-auto absolute left-0 right-0 m-auto"
+        />
         {agenda.map((data, index) => (
           <div
             key={index}
-            className={`flex   ${
+            className={`flex  ${
               index % 2 == 1 ? "justify-end" : "justify-start"
             }`}
           >
@@ -33,6 +43,7 @@ const Agenda = () => {
           </div>
         ))}
       </div>
+      <div className="h-1 bg-[#5D2024] w-full" />
     </div>
   );
 };
