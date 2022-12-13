f385c74b5778d82566edb2942dcfa755436ab7b1
moonayyur <jm_kias@esi.dz>
2022-12-13 01:26:02 +0100

    Fix accordion responsive

---
+++ src/components/faq/Accordion.jsx
@@ -0,0 +1,55 @@
+import { React, useState } from "react";
+import { MdExpandMore } from "react-icons/md";
+import { useSpring, animated } from "react-spring";
+
+function Accordion(props) {
+  const [open, setOpen] = useState(false);
+  const toggleHandler = (e) => {
+    //switch state
+    setOpen(!open);
+  };
+  //open animation with react spring
+
+  const openAnimation = useSpring({
+    from: { maxHeight: "64px" },
+    to: { maxHeight: open ? "400px" : "64px" },
+    config: { duration: "350" },
+  });
+
+  //rotate animation
+  const iconAnimation = useSpring({
+    from: {
+      transform: "rotate(0deg)",
+      color: "#ffff",
+    },
+    to: {
+      transform: open ? "rotate(180deg)" : "rotate(0deg)",
+      color: open ? "#fff" : "#fff",
+    },
+    config: { duration: "50" },
+  });
+  return (
+    <div className="w-[90vw] md:w-[80vw] h- lg:w-[60vw] p-[2px] rounded-lg cursor-pointer bg-gradient-to-t  from-blue to-red">
+      <animated.div
+        className="accordion__item bg-dark rounded-lg p-4 overflow-hidden cursor-pointer opacity-100"
+        style={openAnimation}
+        onClick={toggleHandler}
+      >
+        <div className="accordion__header flex pb-2 sm:pb-1 items-center justify-between p-1">
+          <h4 className="mb-2 font-semibold text-[15.5px] tracking-wider sm:text-[20px] transition ease-in-out delay-200">
+            {props.title}
+          </h4>
+          <animated.div
+            className="transition ease-in-out origin-center mb-2"
+            style={iconAnimation}
+          >
+            <MdExpandMore size={"26px"} enableBackground={"white"} />
+          </animated.div>
+        </div>
+        <p className="accordion__content">{props.text}</p>
+      </animated.div>
+    </div>
+  );
+}
+
+export default Accordion;
--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -1,37 +1,37 @@
-import Accordion from "../components/faq/Accordin";
+import Accordion from "../components/faq/Accordion";
 import { faq } from "../data/data";
 
 const Faq = () => {
-	return (
-		<section
-			id="faq"
-			className="px-8 sm:px-20 lg:px-[12vw] pt-20 min-h-[100vh] flex  justify-center flex-col"
-		>
-			<h1 className="py-5 text-4xl leading-10 font-extrabold text-center md:text-left">
-				FAQ
-			</h1>
-			<div className="w-full px-8 md:px-20 mx-auto space-y-8 flex items-center justify-center flex-col">
-				{faq.map((x, index) => (
-					// <div
-					// 	key={index}
-					// 	className="w-[90vw] md:w-[80vw] lg:w-[60vw] p-[2px] rounded-lg cursor-pointer bg-gradient-to-t  from-blue to-red"
-					// >
-					// 	<details className="bg-dark rounded-lg">
-					// 		<summary className="font-semibold text-white text-[20px] px-4 py-6">
-					// 			{x.question}
-					// 		</summary>
-					// 		<div className="">
-					// 			<p className="text-sm leading-6 text-white px-4 pb-4">
-					// 				{x.answer}
-					// 			</p>
-					// 		</div>
-					// 	</details>
-					// </div>
-					<Accordion title={x.question} text={x.answer} key={index} />
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section
+      id="faq"
+      className="px-8 sm:px-20 lg:px-[12vw] pt-20 min-h-[100vh] flex  justify-center flex-col"
+    >
+      <h1 className="py-5 text-4xl leading-10 font-extrabold text-center md:text-left">
+        FAQ
+      </h1>
+      <div className="w-full px-8 md:px-20 mx-auto space-y-8 flex items-center justify-center flex-col">
+        {faq.map((x, index) => (
+          // <div
+          // 	key={index}
+          // 	className="w-[90vw] md:w-[80vw] lg:w-[60vw] p-[2px] rounded-lg cursor-pointer bg-gradient-to-t  from-blue to-red"
+          // >
+          // 	<details className="bg-dark rounded-lg">
+          // 		<summary className="font-semibold text-white text-[20px] px-4 py-6">
+          // 			{x.question}
+          // 		</summary>
+          // 		<div className="">
+          // 			<p className="text-sm leading-6 text-white px-4 pb-4">
+          // 				{x.answer}
+          // 			</p>
+          // 		</div>
+          // 	</details>
+          // </div>
+          <Accordion title={x.question} text={x.answer} key={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Faq;
