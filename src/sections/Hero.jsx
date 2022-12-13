e999839d640747cda16065c3447bec1a0f3ea195
moonayyur <jm_kias@esi.dz>
2022-12-14 00:31:16 +0100

    Update hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -23,16 +23,18 @@ const Hero = () => {
         <div className="flex justify-center md:justify-start items-center mb-14 w-full">
           <div>
             <motion.h1
-              className=" text-[50px]  md:text-[60px] text-center md:text-left w-full lg:text-[80px] xl:text-[94px] font-extrabold font-libre-franklin"
+              className="text-transparent bg-clip-text bg-gradient-to-b from-[#00f7ff] to-[#ff0004] text-[50px] md:text-[60px] text-center md:text-left w-full lg:text-[80px] xl:text-[94px] font-extrabold font-libre-franklin"
               variants={textVariant(0.4)}
             >
-              Welcome!
+              AI SUMMIT
             </motion.h1>
+
             <motion.p
               className="text-[35px] sm:text-[50px] md:text-[30px] text-center md:text-left lg:text-[32px] xl:text-[48px] leading-[60px]"
               variants={textVariant(0.6)}
             >
-              Show us your data & <br /> we'll show you your future
+              Together,
+              <div className="ml-2">let's reach the summit</div>
             </motion.p>
           </div>
         </div>
